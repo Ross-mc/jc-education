ee02e0ecad480d831e2436beb6281c2099aef4bf
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-05-10 15:04:55 +0100

    removed li that were not inside ul, converted to divs

--- components/Layout/Dropdown/index.js
+++ components/Layout/Dropdown/index.js
@@ -2,10 +2,11 @@ import Link from "next/link";
 import classes from "./dropdown.module.css"
 const Dropdown = ({items}) => {
   return (
+    
     <div className={classes.dropdown}>
     {items.map(item => {
       return (
-        <li key={item.href} className={classes.link}>
+        <div key={item.href} className={classes.link}>
           <div>
             <Link href={item.href} >
               <a>
@@ -15,7 +16,7 @@ const Dropdown = ({items}) => {
               </a>
             </Link>
           </div>
-        </li>
+        </div>
       )
     })}
     </div>
