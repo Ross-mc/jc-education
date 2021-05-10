6c830b3b768440cb4d300d4b85d4da02fcc341fe
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-05-10 14:58:31 +0100

    tidying up serverless auth

--- pages/api/auth/account.js
+++ pages/api/auth/account.js
@@ -2,12 +2,8 @@ import jsonwebtoken from "jsonwebtoken";
 
 export default async (req, res) => {
   if (req.method === "GET"){
-    console.log('inside the get')
-    console.log(req.cookies)
     try {
       const decoded = jsonwebtoken.verify(req.cookies.jwt, process.env.JWT_SECRET);
-      console.log("*********************************************************************")
-      console.log(decoded)
       return res.json({username: decoded.username})
     } catch (error) {
       return res.json({message: "User is not logged in"})
