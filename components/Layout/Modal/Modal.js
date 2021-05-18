0cbcce1de00f9fd92a4132c5ec4ea376217847da
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-05-18 14:07:21 +0100

    changed regex for phone number

--- components/Layout/Modal/Modal.js
+++ components/Layout/Modal/Modal.js
@@ -79,7 +79,7 @@ const Modal = () => {
           <input
             type="tel"
             name="phone"
-            pattern="^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$"
+            pattern="^\s*(([+]\s?\d[-\s]?\d|0)?\s?\d([-\s]?\d){9}|[(]\s?\d([-\s]?\d)+\s*[)]([-\s]?\d)+)\s*$"
             required
             ref={phoneNumberInputRef}
           />
