e951525a3757b89c41afbc3d77d633cfea571c05
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-05-18 16:23:40 +0100

    fixed backend issue with blog - only calls cloudinary if an image is provided otherwise uses a placeholder

--- pages/api/blog.js
+++ pages/api/blog.js
@@ -1,100 +1,104 @@
-import {MongoClient} from "mongodb";
+import { MongoClient } from "mongodb";
 import jsonwebtoken from "jsonwebtoken";
-import {sendImage} from "../../utils/cloudinary"
+import { sendImage } from "../../utils/cloudinary";
 require("dotenv").config();
 
-
 const postHandler = async (blogPost) => {
-  const {title, base64Img, text} = blogPost;
+  const { title, base64Img, text } = blogPost;
   let imageUrl;
-
-  try {
-    imageUrl = await sendImage(base64Img)
-  } catch (error) {
-    throw new Error(error)
+  if (base64Img === "placeholder" || base64Img.length === 0) {
+    imageUrl = "https://res.cloudinary.com/reduce-waste/image/upload/v1621350306/psxiyjschqdvtfgtpmqj.jpg";
+  } else {
+    try {
+      imageUrl = await sendImage(base64Img);
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 
   const newPost = {
     title,
     imageUrl,
     text,
-    date: Date()
-  }
+    date: Date(),
+  };
 
   let connection;
 
   try {
-    connection = await MongoClient.connect(process.env.MONGODB_URI)
+    connection = await MongoClient.connect(process.env.MONGODB_URI);
   } catch (error) {
-    throw new Error("Internal Server Error")
+    throw new Error("Internal Server Error");
   }
-  const db = connection.db()
+  const db = connection.db();
   try {
     const result = await db.collection("blogPosts").insertOne(newPost);
     connection.close();
-    return result
+    return result;
   } catch (error) {
     connection.close();
-    throw new Error("Internal Server Error")
+    throw new Error("Internal Server Error");
   }
-}
+};
 
 const getHandler = async () => {
   let connection;
 
   try {
-    connection = await MongoClient.connect(process.env.MONGODB_URI)
+    connection = await MongoClient.connect(process.env.MONGODB_URI);
   } catch (error) {
-    throw new Error("Internal Server Error")
+    throw new Error("Internal Server Error");
   }
   const db = connection.db();
-  
+
   try {
     const blogPostsCollection = db.collection("blogPosts");
-    const blogPosts = await blogPostsCollection.find({}).sort({date: -1}).toArray();
+    const blogPosts = await blogPostsCollection
+      .find({})
+      .sort({ date: -1 })
+      .toArray();
     connection.close();
-    return blogPosts
+    return blogPosts;
   } catch (error) {
     connection.close();
-    throw new Error("Internal Server Error")
+    throw new Error("Internal Server Error");
   }
-}
+};
 
 const checkIfValidUser = (jwtCookie) => {
   try {
     const decoded = jsonwebtoken.verify(jwtCookie, process.env.JWT_SECRET);
-    return decoded.username ? true : false
+    return decoded.username ? true : false;
   } catch (error) {
-    return false
+    return false;
   }
-}
+};
 
 export default async (req, res) => {
-  if (req.method === "POST"){
+  if (req.method === "POST") {
     const isValidUser = checkIfValidUser(req.cookies.jwt);
-    if (!isValidUser){
-      return res.status(401).json({message: "Not authorised"});
+    if (!isValidUser) {
+      return res.status(401).json({ message: "Not authorised" });
     }
 
-    const {title, base64Img, text} = req.body;
-    if (!title.trim() || !base64Img.trim() || !text.trim()){
-      return res.status(400).json({message: "Invalid Post"})
+    const { title, base64Img, text } = req.body;
+    if (!title.trim() || !base64Img.trim() || !text.trim()) {
+      return res.status(400).json({ message: "Invalid Post" });
     }
     try {
-      const result = await postHandler({title, base64Img, text});
-      return res.status(201).json({message: "Success!"})
+      const result = await postHandler({ title, base64Img, text });
+      return res.status(201).json({ message: "Success!" });
     } catch (error) {
-      return res.status(500).json({message: "Internal Server Error"})
+      return res.status(500).json({ message: "Internal Server Error" });
     }
-  } else if (req.method === "GET"){
-
+  } else if (req.method === "GET") {
     try {
       const blogPosts = await getHandler();
-      return res.status(200).json({blogPosts})
+      return res.status(200).json({ blogPosts });
     } catch (error) {
-      return res.status(500).json({message: "Internal Server Error"})
+      return res.status(500).json({ message: "Internal Server Error" });
     }
   } else {
-    return res.status(405).json({message: "Invalid method"});
+    return res.status(405).json({ message: "Invalid method" });
   }
-}
+};
