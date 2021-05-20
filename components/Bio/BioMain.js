c38485e6d3ec651898359d25585742653cdc58db
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-05-20 16:35:19 +0100

    about page image styling

--- components/Bio/BioMain.js
+++ components/Bio/BioMain.js
@@ -12,12 +12,6 @@ const BioMain = ({ bio }) => {
         </div>
       </div>
       <Bio text={bio[0]}/>
-      {/* <div className="flex-item">
-        <a href="https://www.sweetsquared.com/" target="_blank" rel="noreffer" className={classes.link}>Sweet Squared</a>
-      </div>
-      <div className="flex-item">
-      <a href="https://www.creativeacademymanchester.co.uk/" target="_blank" rel="noreffer" className={classes.link}>Creative Academy Manchester</a>
-      </div> */}
     </div>
     <div className={classes.img}>
       <img src="/images/cnd-solar-oil.jpg" alt="CND Solar Oil"/>
